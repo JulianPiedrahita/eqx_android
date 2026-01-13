a31f06d59c4f537f06ab0077bab3326da20ffe6b
JulianPiedrahita <piedrahitajulian32@gmail.com>
2026-01-13 22:11:55 +0100

    Aumenta memoria Gradle y actualiza configuración de entorno

---
+++ app/build/generated/source/buildConfig/qa/com/example/eqx_android/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.eqx_android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.eqx_android";
+  public static final String BUILD_TYPE = "qa";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from build type: qa
+  public static final String BASE_URL = "https://qa-backend-url";
+}
